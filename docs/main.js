f3da8da06e0b355047db6c54575c9b8c457374ef
Moneyfeet <72102430+Moneyfeet@users.noreply.github.com>
2025-10-21 15:16:41 +0100

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -9,7 +9,7 @@ hamburger.addEventListener('click', () => {
 async function sendMessage() {
   const userInput = document.getElementById("userInput").value;
 
-  const response = await fetch("https://my-chatbot.onrender.com/chat", {
+  const response = await fetch("https://chatbot-api-a65d.onrender.com/chat", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message: userInput })
