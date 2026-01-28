0c48b227f6f44972fc7ca783631fa75d5399e1a7
Moneyfeet <72102430+Moneyfeet@users.noreply.github.com>
2026-01-28 21:01:41 +0000

    Update matches.js

--- docs/lib/Scripts/matches.js
+++ docs/lib/Scripts/matches.js
@@ -5,11 +5,13 @@ fetch("lib/schedule.json")
 
     Object.values(data).forEach(match => {
       const row = document.createElement("tr");
+      const status = match.score1 === '#' || match.score2 === '#' ? 'Upcoming' : (match.score1 > match.score2 ? 'Loss': 'Win');
+      const statusClass = match.score1 === '#' || match.score2 === '#' ? 'upcoming' : (match.score1 > match.score2 ? 'result loss' : 'result win');
       row.innerHTML = `
         <td>${match.datetime}</td>
         <td>${match.opponent}</td>
         <td>${match.score1} - ${match.score2}</td>
-        <td>Unknown</td>
+        <td class="${statusClass}">${status}</td>
       `;
       tbody.appendChild(row);
     });
